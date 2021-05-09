6dae7443f8be5c89c229171ecf413a9e6447cf5d
pegman999 <72978844+pegman999@users.noreply.github.com>
2021-05-09 15:40:49 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,41 +1,51 @@
 //Initialising 
 const express = require('express');
-const path = require('path');
 const router = require('./routes/fitnessGoalsRoutes'); 
-
-//Initialising mustache and bodyParser
+const path = require('path');
+const nedb = require('nedb');
 const mustache = require('mustache-express');
+const auth = require('./auth/auth'); 
+const passport = require('passport');
+const session = require('express-session');
+
 const bodyParser = require('body-parser');
+
+const db = new nedb();
 const app = express();
+
 const public = path.join(__dirname, 'public');
 
-//Initial router to main page under "/"
-app.use(bodyParser.urlencoded({extended:false}));
-app.use('/', router);
-app.use(express.static(public));
 
-
-//Engine for Mustache
 app.engine('mustache', mustache());
 app.set('view engine', 'mustache');
 
-//Route used 404 when unrecognized route
-router.use(function(req, res) {
-    res.status(404);
-    res.type('text/plain');
-    res.send('404 Not found.');
-   })
-   
-//Route to port 500 to show an internal server error
-router.use(function(err, req, res, next) {
-res.status(500);
-res.type('text/plain');
-res.send('Internal Server Error.');
-})
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:false}));
+app.use(session({secret: 'Secret', resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(public));
 
+auth.init();
+
+
+app.use('/', router);
 
 
 //Allows the app to be connected on port 3000
 app.listen(3000, () => {
- console.log('Server started on port 3000. Ctrl^c to quit.');
+    console.log('Server started on port 3000. Ctrl^c to quit.');
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
--- controllers/fitnessGoalsControllers.js
+++ controllers/fitnessGoalsControllers.js
@@ -1,12 +1,21 @@
 const fitnessGoalsDAO = require('../models/fitnessGoalsModel'); 
 const db = new fitnessGoalsDAO('goals.db'); 
+const userDao = require('../models/userModel.js');
+const passport = require('passport');
 
-//Controller for inital page of site (Take all entries from model page and displays to console)
-exports.landing_page = function(req, res) {
+//Controller for landing page
+exports.landing_page = function(req, res){
+res.render('homepage', {
+    'title': 'Fitness Goals',
+})}
+
+//Controller for to go to entries page
+exports.goals_page = function(req, res) {
     db.getAllEntries().then((list) => {
         res.render('entries', {
             'title': 'Fitness Goals',
-            'entries': list
+            'entries': list,
+            "user": req.user
         })
         console.log('fitnessGoals_entries uses all messages');
         console.log(list);
@@ -25,24 +34,25 @@ exports.entries_list = function(req, res) {
 
 
 //Controller use to display the title and new entries
-exports.show_new_entries = function(req, res){
-    res.render('newEntry',{
-        'title': 'Fitness Goals'
+exports.show_new_entries = function(req, res) {
+    res.render('newEntry', {
+    'title': 'Guest Book',
+    'user': req.user
     })
-}
+   } 
 
 //Controller which extracts data from input and adds to database then redirects to the home page with new data
 exports.post_new_entry = function(req, res){
     console.log('extracted data:', req.body.name, req.body.goals, req.body.date, req.body.duration, req.body.completed);
     db.addEntry(req.body.name, req.body.goals, req.body.date, req.body.duration, req.body.completed)
-    res.redirect('/');
+    res.redirect('/goalspage');
 }
 
 //Controller which deletes entries and redirects back to the home page
 exports.remove_new_entry = async function(req, res) {
     const id = req.params._id;
     await db.deleteGoal(id);
-    res.redirect(req.baseUrl + '/');
+    res.redirect(req.baseUrl + '/goalspage');
 }
 
 
@@ -57,7 +67,7 @@ exports.show_updated_entry = function(req, res){
 exports.post_updated_entry= function(req, res){
     console.log('id in update entry', req.params.id);
     db.updateEntry(req.params.id, req.body.name, req.body.goals, req.body.date, req.body.duration, req.body.completed);
-    res.redirect('/');
+    res.redirect('/goalspage');
 }
 
 //Controller to show the finished goals
@@ -85,3 +95,69 @@ exports.uncomplete_page = function(req, res) {
     }).catch((err)=>{
         console.log('promise rejcted', err)
     })}
+
+
+//Controller to go to register page
+exports.show_register_page = function(req, res){
+    res.render("user/register");
+    }
+
+//Controller to post the new user
+exports.post_new_user = function(req, res) {
+    const user = req.body.username;
+    const password = req.body.pass;
+    //console.log("register user", user, "password", password);
+    if (!user || !password) {
+    res.send(401, 'no user or no password');
+    return;
+    }
+    userDao.lookup(user, function(err, u) {
+    if (u) {
+    res.send(401, "User exists:", user);
+    return;
+    }
+    userDao.create(user, password);
+    res.redirect('/login');
+    });
+    } 
+
+//Controller to route to the login page
+exports.show_login_page = function(req, res) {
+    res.render('user/login');
+    };
+
+//Controller to redirect to the homepage
+exports.post_login = function(req, res) {
+    console.log('serialUser wrote', req.session.passport.user);
+    res.redirect('/goalspage');
+    }; 
+
+
+//Controller to show the user entries
+exports.show_user_entries = function(req, res) {
+    let user = req.params.author;
+    db.getEntriesByUser(user)
+    .then((entries) => {
+    res.render('entries', {
+    'title': 'Guest Book',
+    'user': req.user,
+    'entries': entries
+    });
+    })
+    .catch((err) => {
+    console.log('Error: ')
+    console.log(JSON.stringify(err))
+    });
+    } 
+
+//Controller to the logout page
+exports.logout = function(req, res) {
+    req.logout();
+    res.redirect('/');
+}; 
+
+
+exports.authorize = function(redirect) {
+    return passport.authenticate('local',
+{ failureRedirect: redirect});
+}; 
